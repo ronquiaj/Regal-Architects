062fd90d1275d80c94782babbc5121fe39f1482b
Adrian Ronquillo <ronquiaj@plu.edu>
2021-02-16 21:10:29 -0900

    --

---
+++ index.js
@@ -0,0 +1,30 @@
+const changePhoto = () => {
+  document.querySelector(".partner--4").classList.remove("visible");
+  document.querySelector(".partner--4").classList.add("transitioning");
+  document.querySelector(".partner--1").classList.add("visible");
+  setTimeout(() => {
+    document.querySelector(".partner--4").classList.remove("transitioning");
+    document.querySelector(".partner--1").classList.add("transitioning");
+    document.querySelector(".partner--1").classList.remove("visible");
+    document.querySelector(".partner--2").classList.add("visible");
+  }, 7500);
+
+  setTimeout(() => {
+    document.querySelector(".partner--2").classList.add("transitioning");
+    document.querySelector(".partner--1").classList.remove("transitioning");
+    document.querySelector(".partner--2").classList.remove("visible");
+    document.querySelector(".partner--3").classList.add("visible");
+  }, 15000);
+
+  setTimeout(() => {
+    document.querySelector(".partner--3").classList.add("transitioning");
+    document.querySelector(".partner--2").classList.remove("transitioning");
+    document.querySelector(".partner--3").classList.remove("visible");
+    document.querySelector(".partner--4").classList.add("visible");
+  }, 22500);
+};
+
+changePhoto();
+setInterval(() => {
+  changePhoto();
+}, 30000);
